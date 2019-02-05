e8e83a044970b50a0660c0fb3351a39da7b909c1
Gentry Atkinson <gma23@txstate.edu>
2019-02-05 15:50:26 -0600

    submission script working. building more questions

--- process.php
+++ process.php
@@ -9,7 +9,7 @@
 		$formdata = array(
 			'userCommand'=> $_POST['userCommand'],
 			'originalCommand'=> $_POST['originalCommand'],
-			'originalQuantity'=>$_POST['originalQuantity']
+			'originalQuantity'=> $_POST['originalQuantity']
 		);
 		$switchVar = $_POST['page'];
 		
@@ -18,14 +18,18 @@
 		$outString .= $formdata['originalCommand'];
 		$outString .= "\", \"originalQuantity\" : \"";
 		$outString .= $formdata['originalQuantity'];
+                $outString .= "\", \"date\" : \"";
+		$outString .= date("Y/m/d");
+                $outString .= "\", \"time\" : \"";
+		$outString .= date("H:i:s");
 		$outString .= "\"}\n";
 
 		if (is_writable("logoCommands.json")){		
 			$outFile = fopen("logoCommands.json", "a");
 			fwrite($outFile, $outString);
 			fclose($outFile);
-			echo "This page is ";
-			echo $switchVar;
+			//echo "This page is ";
+			//echo $switchVar;
 		}
 		else{
 			echo "File is not writable.";
@@ -33,15 +37,18 @@
 
 		switch($switchVar){
 			case 1:
-				//redirect('survey2.html');
-				echo "Caught 1";
+				header("Location: https://userweb.cs.txstate.edu/~gma23/survey2.html");
+				break;
+                        case 2:
+				header("Location: https://userweb.cs.txstate.edu/~gma23/survey3.html");
 				break;
 			default:
-				echo "Bad page number."
+				echo "Bad page number.";
 		}
 	}
 	catch (Exception $e) {
 	    echo 'Caught exception: ',  $e->getMessage(), "\n";
 	}
+        exit();
 
 ?>
